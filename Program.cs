d6f7bbfae1d4a5efc0eead2b4a165887f0f45318
hp <hp@DESKTOP-F9FBMU7>
2023-03-20 13:01:56 +0300

    end of task 3

--- Program.cs
+++ Program.cs
@@ -16,6 +16,21 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
 });
 
+
+/////
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
+/////
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
